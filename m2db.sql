827a0b22e6d05b34168e31ac628710d632673f1e
Anoopksingh123 <99683335+Anoopksingh123@users.noreply.github.com>
2022-02-23 13:01:18 +0530

    Add files via upload

---
+++ m2db.sql
@@ -0,0 +1,10 @@
+﻿select *from Location
+insert into Location (Place,Detail)values('noida','this is nice place')
+select *from customer
+insert into Customer values('Shubham','Shubham@gmial.com',2),
+('Anoop','anoop@gmial.com',1),
+('Alok','alok@gmial.com',3),
+('Himanshu','himanshu@gmial.com',2)
+select *from Location
+select *from customer
+select *from Location
